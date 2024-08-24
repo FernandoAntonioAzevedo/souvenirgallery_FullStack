6772b01f88ede58cd44b44c6b0ace9defec76a06
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-08-24 01:33:56 -0300

    iniciando componentização do projeto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,36 @@
+import { Outlet } from "react-router-dom";
 
-import './App.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
 
 function App() {
-  
-  return ( 
+  return (
     <div className="App">
-      <h1>Souvenir Gallery</h1>
-
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Navbar />
+      <div className="container">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-
-  )
+  );
 }
 
 export default App;
